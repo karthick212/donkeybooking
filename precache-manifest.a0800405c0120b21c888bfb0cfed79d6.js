c0e8370467f814334e7059b3fda97141056edd66
karthick212 <karthick212>
2019-12-10 09:44:19 +0530

    Fist

---
+++ precache-manifest.a0800405c0120b21c888bfb0cfed79d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55814cf4f9c9ad93dc60ebc401380d34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d59b1c7a3d0b8d78b187",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f219210606d2f884ff48",
+    "url": "/static/js/2.153eb87b.chunk.js"
+  },
+  {
+    "revision": "d59b1c7a3d0b8d78b187",
+    "url": "/static/js/main.11d063ba.chunk.js"
+  },
+  {
+    "revision": "2117e8916e61a4c68f2d",
+    "url": "/static/js/runtime-main.cceac069.js"
+  }
+]);
