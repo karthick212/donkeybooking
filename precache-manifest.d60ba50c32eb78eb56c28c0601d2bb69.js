138b73024df0f9076b32890ccf3a37ebed15928e
karthick212 <karthick212>
2020-02-02 13:10:09 +0530

    Updates

---
+++ precache-manifest.d60ba50c32eb78eb56c28c0601d2bb69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6606f4a452af41f24b27513d5de420ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4922e4f7d86ab9fa6e3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df9caf67bae295876bb6",
+    "url": "/static/js/2.436be81a.chunk.js"
+  },
+  {
+    "revision": "e4922e4f7d86ab9fa6e3",
+    "url": "/static/js/main.f3049e9c.chunk.js"
+  },
+  {
+    "revision": "2117e8916e61a4c68f2d",
+    "url": "/static/js/runtime-main.cceac069.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Booking/precache-manifest.0d5e46586c7a7514e609682f10ae1ea5.js"
+  "/precache-manifest.d60ba50c32eb78eb56c28c0601d2bb69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Booking/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
