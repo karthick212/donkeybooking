16b5126f4defab935d4c2c8b05ae854b6bd5e32b
karthick212 <karthick212>
2020-08-08 18:32:03 +0530

    Updates

---
+++ precache-manifest.9a1dc4ad65470a5ddf52204b7571049d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5531ad840c6d312aef5ca86cda09f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cd4a35017492447514",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f2432d8af8060a4a6c78",
+    "url": "/static/js/2.dec6ca68.chunk.js"
+  },
+  {
+    "revision": "66cd4a35017492447514",
+    "url": "/static/js/main.e09509ac.chunk.js"
+  },
+  {
+    "revision": "2117e8916e61a4c68f2d",
+    "url": "/static/js/runtime-main.cceac069.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90a35c5dc36d8d9ebddea028a5e044f5.js"
+  "/precache-manifest.9a1dc4ad65470a5ddf52204b7571049d.js"
 );
 
 self.addEventListener('message', (event) => {
